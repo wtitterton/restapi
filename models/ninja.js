31e188d284a50824c911c8df1708e0c330f4228e
wtitterton <wftitterton@gmail.com>
2018-12-30 14:19:29 +0000

    created model and schema for ninjas

---
+++ models/ninja.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create ninjs schema and model
+const NinjaSchema = new Schema({
+    name: {
+        type:String,
+        required:[true, "Name field ins required"],
+    },
+    rank: {
+        type: String
+    },
+    available:{
+        type: Boolean,
+        default:false
+    }
+    // add in geo location
+})
+
+const Ninja = mongoose.model('ninja', NinjaSchema );
+
+module.exports = Ninja;
